1966fa0adf4acde5cdb05880cc87bdbda73a4d73
ZAHID GUL <98535146+zahidgul107@users.noreply.github.com>
2024-03-05 17:22:31 +0530

    search in 2D array

---
+++ linear_search_algorithm/src/linear_search_algorithm/linear/FindMin.java
@@ -0,0 +1,21 @@
+package linear_search_algorithm.linear;
+
+public class FindMin {
+
+	public static void main(String[] args) {
+		int [] arr = {18, 12, -7, 2, -14, 28};
+		System.err.println(min(arr));
+
+	}
+	
+	static int min(int[] arr) {
+		int ans = arr[0];
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] < ans) {
+				ans = arr[i];
+			}
+		}
+		return ans;
+	}
+
+}
