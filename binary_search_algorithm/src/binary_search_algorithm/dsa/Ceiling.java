6d432de6e9219f857c08590d176b2d3299bdc53c
ZAHID GUL <98535146+zahidgul107@users.noreply.github.com>
2024-04-03 20:04:09 +0530

    ceiling and floor of number

---
+++ binary_search_algorithm/src/binary_search_algorithm/dsa/Ceiling.java
@@ -0,0 +1,35 @@
+package binary_search_algorithm.dsa;
+
+public class Ceiling {
+	
+	public static void main(String[] args) {
+		int[] arr = { 2, 3, 5, 9, 14, 16, 18 };
+		int target = 19;
+		int ans = ceiling(arr, target);
+		System.err.println(ans);
+		
+	}
+	
+//	ceiling -> the index of smallest no >= target
+	static int ceiling(int[] arr, int target) {
+		
+		if (target > arr[arr.length - 1]) {
+			return -1;
+		}
+		int start = 0;
+		int end = arr.length;
+
+		while (start <= end) {
+			int mid = start + (end - start) / 2;
+
+			if (target < arr[mid])
+				end = mid - 1;
+			else if (target > arr[mid])
+				start = mid + 1;
+			else
+				return mid;
+		}
+		return start;
+	}
+
+}
