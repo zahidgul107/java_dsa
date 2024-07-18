1d20de2d898b7812803f414c70e075a1e1c8e7d9
ZAHID GUL <98535146+zahidgul107@users.noreply.github.com>
2024-07-18 17:20:22 +0530

    pattern questions

--- pattern_questions/src/pattern_questions/Main.java
+++ pattern_questions/src/pattern_questions/Main.java
@@ -3,13 +3,62 @@ package pattern_questions;
 public class Main {
 	
 	public static void main(String args[]) {
-		pattern1(50);
+	//	pattern1(4);
+	//	pattern2(4);
+	//	pattern3(4);
+		pattern4(6);
+
 	}
 	
 	static void pattern1(int n) {
 		for (int row = 1; row <= n; row++) {
+			for (int col = 1; col <= n; col++) {
+				System.err.print("* ");
+			}
+			System.err.println();
+		}
+	}
+	
+	static void pattern2(int n) {
+		for (int row = 1; row <= n; row++) {
 			for (int col = 1; col <= row; col++) {
 				System.err.print("* ");
+			}
+			System.err.println();
+		}
+	}
+	
+//	static void pattern3(int n) {
+//		for (int row = 1; row <= n; row++) {
+//			for (int col = 1; col <= n-row+1; col++) {
+//				System.err.print("*");
+//			}
+//			System.err.println();
+//		}
+//	}
+	
+	static void pat(int p) {
+		for(int row = 1; row <= p; row++) {
+			for(int col = 1; col <= p-row+1; col++) {
+				System.err.print("* ");
+			}
+			System.err.println();
+		}
+	}
+	
+	static void pattern3(int n) {
+		for (int row = 1; row <= n; row++) {
+			for (int col = n; col >= row; col--) {
+				System.err.print("*");
+			}
+			System.err.println();
+		}
+	}
+	
+	static void pattern4(int n) {
+		for (int row = 1; row <= n; row++) {
+			for (int col = 1; col <= row; col++) {
+				System.err.print(col + " ");
 			}
 			System.err.println();
 		}
