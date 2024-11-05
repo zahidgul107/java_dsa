e019615b52a88bb2c30489e08fda7a2321200e43
ZAHID GUL <98535146+zahidgul107@users.noreply.github.com>
2024-11-05 15:17:40 +0530

    reverse array, Remove duplicate element

--- InterviewQuestion/src/question/ReverseArray.java
+++ InterviewQuestion/src/question/ReverseArray.java
@@ -8,8 +8,8 @@ public class ReverseArray {
 		
 		int[] arr = {2, 11, 5, 10, 7, 8};
 	//	System.err.println(Arrays.toString(arr));
-		System.err.println(Arrays.toString(reverse(arr)));
-		
+	//	System.err.println(Arrays.toString(reverse(arr)));
+		System.err.println(Arrays.toString(reverseArray(arr, 0, arr.length - 1)));
 	}
 	
 	public static int[] reverse(int[] arr) {
@@ -22,6 +22,16 @@ public class ReverseArray {
 		}
 		
 		return result;
+	}
+	
+	public static int[] reverseArray(int[] arr, int start, int end) {
+		
+		while(start < end) {
+			int temp = arr[start];
+			arr[start++] = arr[end];
+			arr[end--] = temp;
+		}
+		return arr;
 	}
 	
 	
