89959c8f84eb06fb517ab3fec13d6ba410dd159f
ZAHID GUL <98535146+zahidgul107@users.noreply.github.com>
2024-11-13 16:46:56 +0530

    created singlyLinkedList and printing it

---
+++ InterviewQuestion/src/question/SinglyLinkedList.java
@@ -0,0 +1,44 @@
+package question;
+
+public class SinglyLinkedList {
+	
+	private ListNode head;
+	
+	public static class ListNode{
+		private int data;
+		private ListNode next;
+		
+		public ListNode(int data) {
+			this.data = data;
+			this.next = null;
+		}
+	}
+	
+	public void display() {
+		ListNode current = head;
+		while(current != null) {
+			System.out.print(current.data + " --> ");
+			current = current.next;
+		}
+		
+		System.out.print("null");
+	}
+	
+	public static void main(String[] args) {
+		
+		SinglyLinkedList sll = new SinglyLinkedList();
+		sll.head = new ListNode(10);
+		ListNode second = new ListNode(1);
+		ListNode third = new ListNode(8);
+		ListNode fourth = new ListNode(11);
+		
+		// Now we will connect them together to form a chain
+		sll.head.next = second;  // 10 --> 1
+		second.next = third; // 10 --> 1 --> 8
+		third.next = fourth; // 10 --> 1 --> 8 --> 11 --> null
+		
+		sll.display();
+		
+	}
+
+}
