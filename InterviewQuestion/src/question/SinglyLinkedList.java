bdf1b56114431eed20da6deeea28dff53e7a7567
ZAHID GUL <98535146+zahidgul107@users.noreply.github.com>
2024-11-14 10:46:00 +0530

    length of singlyLinkedList

--- InterviewQuestion/src/question/SinglyLinkedList.java
+++ InterviewQuestion/src/question/SinglyLinkedList.java
@@ -24,6 +24,21 @@ public class SinglyLinkedList {
 		System.out.print("null");
 	}
 	
+	public int length() {
+		if(head == null) {
+			return 0;
+		}
+		
+		int count = 0;
+		ListNode current = head;
+		while(current != null) {
+			count++;
+			current = current.next;
+		}
+		
+		return count;
+	}
+	
 	public static void main(String[] args) {
 		
 		SinglyLinkedList sll = new SinglyLinkedList();
@@ -38,6 +53,7 @@ public class SinglyLinkedList {
 		third.next = fourth; // 10 --> 1 --> 8 --> 11 --> null
 		
 		sll.display();
+		System.err.println("Length : " + sll.length());
 		
 	}
 
