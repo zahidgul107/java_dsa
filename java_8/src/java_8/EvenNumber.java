4ee0db30b5ce2b21c66f36d8c2aa11155fadddf1
ZAHID GUL <98535146+zahidgul107@users.noreply.github.com>
2025-02-17 12:21:21 +0530

    even using stream api

---
+++ java_8/src/java_8/EvenNumber.java
@@ -0,0 +1,14 @@
+package java_8;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class EvenNumber {
+	
+	public static void main(String args[]) {
+		List<Integer> list = List.of(2, 4, 5 ,7, 8);
+		List<Integer> evenList =  list.stream().filter(n -> n % 2 == 0).collect(Collectors.toList());
+		System.err.println(evenList);
+	}
+
+}
