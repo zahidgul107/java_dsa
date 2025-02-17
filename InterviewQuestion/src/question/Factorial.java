82d12e01a34524f32fd833ffe8a6180221ec6215
ZAHID GUL <98535146+zahidgul107@users.noreply.github.com>
2025-02-17 11:06:44 +0530

    factorial using recursion method

--- InterviewQuestion/src/question/Factorial.java
+++ InterviewQuestion/src/question/Factorial.java
@@ -3,7 +3,14 @@ package question;
 public class Factorial {
 	
 	public static void main(String[] args) {
-		
+		System.err.println(factorial(5));
+	}
+	
+	public static int factorial(int n) {
+		if(n == 0) {
+			return 1;
+		}
+		return n * factorial(n - 1);
 	}
 
 }
