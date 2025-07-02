fcb064bd1baa6cb9831439774ceaa97457e88618
ZAHID GUL <98535146+zahidgul107@users.noreply.github.com>
2025-07-02 20:42:21 +0530

    counting numbers

--- InterviewQuestion/src/question/CountingString.java
+++ InterviewQuestion/src/question/CountingString.java
@@ -1,5 +1,17 @@
 package question;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class CountingString {
+	
+	public static void main(String[] args) {
+		List<String> list = Arrays.asList("apple", "bat", "cat", "banana", "dog");
+		Map<Integer, Long> strLenMap = list.stream().collect(Collectors.groupingBy(String::length, Collectors.counting()));
+		
+		System.err.println(strLenMap);
+	}
 
 }
