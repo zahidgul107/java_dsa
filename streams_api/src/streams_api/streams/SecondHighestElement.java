aa0f40dafe983f2c87125ede06e0025cadd53092
ZAHID GUL <98535146+zahidgul107@users.noreply.github.com>
2025-07-09 12:29:20 +0530

    longest word, secondHightestElement

--- streams_api/src/streams_api/streams/SecondHighestElement.java
+++ streams_api/src/streams_api/streams/SecondHighestElement.java
@@ -1,17 +1,18 @@
 package streams_api.streams;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Optional;
 
 public class SecondHighestElement {
 
 	public static void main(String[] args) {
-		int[] x = {1, 2, 5, 6, 3, 8, 7};
+		int[] x = {1, 2, 5, 6, 6, 3, 8, 8, 8, 7, 7};
 		System.err.println(secondHighest(x));
 	}
 
 	private static Optional<Integer> secondHighest(int[] x) {
-		return Arrays.stream(x).boxed().skip(1).findFirst();
+		return Arrays.stream(x).boxed().sorted(Comparator.reverseOrder()).distinct().skip(1).findFirst();
 	}
 
 }
