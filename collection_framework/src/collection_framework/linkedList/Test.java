4201c253a4785b3284112321c9ae7361c1528e7e
ZAHID GUL <98535146+zahidgul107@users.noreply.github.com>
2025-07-14 15:58:13 +0530

    LinkedList

---
+++ collection_framework/src/collection_framework/linkedList/Test.java
@@ -0,0 +1,52 @@
+package collection_framework.linkedList;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		Node node1 = new Node();
+		Node node2 = new Node();
+		
+		node1.value = 1;
+		node2.value = 2;
+		
+		node1.next = node2;
+		node2.next = null;
+		
+		LinkedList<Integer> linkedList = new LinkedList<>();
+		linkedList.add(1);
+		linkedList.add(2);
+		linkedList.add(3);
+		linkedList.get(2);  	// 0(n)
+		linkedList.addLast(4); 	// 0(1)
+		linkedList.addFirst(0); // 0(2)
+		linkedList.getFirst();
+		linkedList.getLast();
+		
+		System.err.println(linkedList);
+		
+		linkedList.removeIf(n -> n % 2 == 0);
+		
+		System.err.println(linkedList);
+		
+		LinkedList<String> animals = new LinkedList<>(Arrays.asList("Cat", "Dog", "Elephant"));
+		LinkedList<String> animalsToRemove = new LinkedList<>(Arrays.asList("Dog", "Lion"));
+		
+		animals.removeAll(animalsToRemove);
+		
+		System.err.println(animals);
+		
+	}
+
+}
+
+class Node {
+	
+	public int value;
+	
+	public Node next;
+}
