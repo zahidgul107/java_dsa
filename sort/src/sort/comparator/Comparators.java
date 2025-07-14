e3b11b9ec19cbee27f18909397e89d41d67e61ee
ZAHID GUL <98535146+zahidgul107@users.noreply.github.com>
2025-07-14 12:12:51 +0530

    compare students using comparator

--- sort/src/sort/comparator/Comparators.java
+++ sort/src/sort/comparator/Comparators.java
@@ -1,7 +1,10 @@
 package sort.comparator;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Comparators {
 
@@ -13,4 +16,64 @@ public class Comparators {
 
 	}
 
+}
+
+class Student {
+	
+	private String name;
+	
+	private double gpa;
+	
+	public Student(String name, double gpa) {
+		this.name = name;
+		this.gpa = gpa;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public double getGpa() {
+		return gpa;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setGpa(double gpa) {
+		this.gpa = gpa;
+	}
+	
+}
+
+class Main {
+	
+	public static void main(String[] args) {
+		List<Student> list = new ArrayList<>();
+		list.add(new Student("Charlie", 3.5));
+		list.add(new Student("Alice", 3.5));
+		list.add(new Student("Bob", 3.7));
+		
+		list.add(new Student("Akshit", 3.9));
+		
+		Comparator<Student> comparator = Comparator.comparing(Student::getGpa).reversed().thenComparing(Student::getName);
+		list.sort(comparator);
+		
+	/*	list.sort((a, b) -> {
+			if(b.getGpa() - a.getGpa() > 0)
+				return 1;
+			else if(b.getGpa() - a.getGpa() < 0)
+				return -1;
+			else
+				return a.getName().compareTo(b.getName());
+		});  */
+		list.forEach(s ->
+			System.err.println(s.getName() + " :" + s.getGpa())
+		);
+		
+		String res = list.stream().map(Student::toString).collect(Collectors.joining(", "));
+		System.err.println(res);
+	}
+	
 }
